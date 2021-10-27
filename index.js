6dc26de451007874b2694b9b3b9bbee02c409fa3
HassenH1 <hasansaid51@gmail.com>
2021-10-26 22:08:30 -0700

    experiment gone wrong lol

---
+++ index.js
@@ -0,0 +1,32 @@
+const tooltip = document.querySelector("#tooltip");
+tooltip.style.display = "none";
+
+window.addEventListener("scroll", (event) => {
+  let scroll = this.scrollY;
+  if (scroll >= 230) {
+    // tooltip.style.opacity = 1.0;
+    fade();
+    tooltip.style.display = "block";
+  }
+});
+
+async function fade() {
+  if (tooltip.style.opacity >= 0.5) {
+    return;
+  }
+  for (let i = 9; i >= 1; i--) {
+    await sleep(1000);
+
+    let num = 1 / i;
+
+    tooltip.style.opacity = num;
+    // if (i === 0.9) {
+    //   i = 1;
+    // }
+    console.log(num, "<=-=what is i?");
+  }
+}
+
+function sleep(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds));
+}
