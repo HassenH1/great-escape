58f0ed95f782b0e1077ce434f74decce14730721
HassenH1 <hasansaid51@gmail.com>
2021-10-30 10:59:17 -0700

    removed modal for now

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-let ini = document.querySelector("#modal-window");
-ini.classList.add("hideModal");
+// let ini = document.querySelector("#modal-window");
+// ini.classList.add("hideModal");
 
-function openModal() {
-  let modal = document.querySelector("#modal-window");
-  modal.classList.add("showModal");
-}
+// function openModal() {
+//   let modal = document.querySelector("#modal-window");
+//   modal.classList.remove("hideModal");
+//   modal.classList.add("showModal");
+// }
 
-function closeM() {
-  let m = document.querySelector("#modal-window");
-  m.classList.remove("showModal");
-}
+// function closeM() {
+//   let m = document.querySelector("#modal-window");
+//   m.classList.add("hideModal");
+//   m.classList.remove("showModal");
+// }
